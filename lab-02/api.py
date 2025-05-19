60c02f937be697a7caa1a81c5b8b471f4565f47d
Sawasaki-coder <nn436223@gmail.com>
2025-05-19 10:35:05 +0700

    [add] lab-02

--- lab-02/api.py
+++ lab-02/api.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher  # Thêm vào phần đầu của file api.py
 from cipher.railfence import RailFenceCipher
+from cipher.playfair import PlayFairCipher  
+
 app = Flask(__name__)
 # Thêm đoạn sau vào trước hàm main
 
@@ -13,6 +15,9 @@ vigenere_cipher = VigenereCipher()
 
 # RAILFENCE CIPHER ALGORITHM
 railfence_cipher = RailFenceCipher()
+
+# PLAYFAIR CIPHER ALGORITHM
+playfair_cipher = PlayFairCipher()
 
 #caesar
 @app.route("/api/caesar/encrypt", methods=["POST"])
@@ -65,6 +70,33 @@ def decrypt():
     key = int(data['key'])
     decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
+
+#playfair
+@app.route('/api/playfair/creatematrix', methods=['POST'])
+def playfair_creatematrix():
+    data = request.json
+    key = data['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    return jsonify({"playfair_matrix": playfair_matrix})
+
+@app.route('/api/playfair/encrypt', methods=['POST'])
+def playfair_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = data['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    encrypted_text = playfair_cipher.playfair_encrypt(plain_text, playfair_matrix)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/playfair/decrypt', methods=['POST'])
+def playfair_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = data['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    decrypted_text = playfair_cipher.playfair_decrypt(cipher_text, playfair_matrix)
+    return jsonify({'decrypted_text': decrypted_text})
+
 #main function
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port = 5000, debug = True)
